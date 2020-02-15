fd877ee04f673e6e4054fbc3fb4781db4938a0b0
ogulab <ogura.guri@gmail.com>
2020-02-15 18:45:32 +0900

    update LEDの点灯状態を出力

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.OUT)
 for x in xrange(5):
     GPIO.output(4,True)
+    print('点灯')
     time.sleep(2)
     GPIO.output(4,False)
+    print('消灯')
     time.sleep(2)
 GPIO.cleanup()
 
