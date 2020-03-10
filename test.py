dfa869721e5dc1640065467ea35edf22efd1e868
ogulab <ogura.guri@gmail.com>
2020-03-10 20:49:18 +0900

    update 辞書の使い方

--- test.py
+++ test.py
@@ -13,26 +13,7 @@ for x in xrange(5):
 GPIO.cleanup()
 print('終了')
 
-def getTime():
-    n=time.strftime('%I')+time.strftime('%M')
-    timeList=list(n)
-    return timeList
 
-def setAmPm
-    ap =time.strftime('%p')
-    if ap =='AM':
-        turnlight(20,1)
-        turnLight(21,0)
-    else:
-        turnLigut(20,0)
-        turnLight(21,1)
-
-def switchLight(gpio,number):
-    GPIO.setup(gpio,GPIO.OUUT)
-    if number == 0 :
-        GPIO.output(gpio,GPIO.HIGH)
-    else:
-        GPIO.output(gpio,GPIO.LOW)
-
-def lightNumber(number,cnt):
-    
+#辞書
+books={"小説":"a","料理":"b","辞書":"c"}
+print(books["料理"])    
