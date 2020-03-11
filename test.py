2dfb68dd5efabcbb3e3f869836767a7f5affe36b
ogulab <ogura.guri@gmail.com>
2020-03-11 20:29:10 +0900

    update

--- test.py
+++ test.py
@@ -17,3 +17,8 @@ print('終了')
 #辞書
 books={"小説":"a","料理":"b","辞書":"c"}
 print(books["料理"])    
+
+#文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+originalstr='stressed'
+newstrlist=list(reversed(originalstr))
+print(newstrlist)
